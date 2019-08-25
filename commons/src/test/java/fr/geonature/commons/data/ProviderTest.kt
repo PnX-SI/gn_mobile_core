2f17148e6723153dd6ec5b641c11087b1f662f09
S. Grimault <sebastien.grimault@gmail.com>
2019-08-25 16:56:36 +0200

    chore: upgrade 'Robolectric' library dependency

---
+++ commons/src/test/java/fr/geonature/commons/data/ProviderTest.kt
@@ -0,0 +1,30 @@
+package fr.geonature.commons.data
+
+import fr.geonature.commons.data.Provider.buildUri
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+/**
+ * Unit tests about [Provider].
+ *
+ * @author [S. Grimault](mailto:sebastien.grimault@gmail.com)
+ */
+@RunWith(RobolectricTestRunner::class)
+class ProviderTest {
+
+    @Test
+    fun testBuildUri() {
+        assertEquals("content://${Provider.AUTHORITY}/taxa",
+                     buildUri("taxa").toString())
+
+        assertEquals("content://${Provider.AUTHORITY}/taxa/123",
+                     buildUri("taxa",
+                              123.toString()).toString())
+
+        assertEquals("content://${Provider.AUTHORITY}/taxa/area/123",
+                     buildUri("taxa",
+                              "area/${123}").toString())
+    }
+}
