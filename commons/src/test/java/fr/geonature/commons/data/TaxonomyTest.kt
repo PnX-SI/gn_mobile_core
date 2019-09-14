fa1db495c12c10a396c11d582c1952b74d1aa891
S. Grimault <sebastien.grimault@gmail.com>
2019-09-14 15:40:11 +0200

    feat: taxonomy

---
+++ commons/src/test/java/fr/geonature/commons/data/TaxonomyTest.kt
@@ -0,0 +1,67 @@
+package fr.geonature.commons.data
+
+import android.database.Cursor
+import android.os.Parcel
+import fr.geonature.commons.data.Taxonomy.Companion.fromCursor
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
+import org.robolectric.RobolectricTestRunner
+
+/**
+ * Unit tests about [Taxonomy].
+ *
+ * @author [S. Grimault](mailto:sebastien.grimault@gmail.com)
+ */
+@RunWith(RobolectricTestRunner::class)
+class TaxonomyTest {
+
+    @Test
+    fun testEquals() {
+        assertEquals(Taxonomy("Animalia",
+                              "Ascidies"),
+                     Taxonomy("Animalia",
+                              "Ascidies"))
+    }
+
+    @Test
+    fun testCreateFromCursor() {
+        // given a mocked Cursor
+        val cursor = mock(Cursor::class.java)
+        `when`(cursor.getColumnIndexOrThrow(Taxonomy.COLUMN_KINGDOM)).thenReturn(0)
+        `when`(cursor.getColumnIndexOrThrow(Taxonomy.COLUMN_GROUP)).thenReturn(1)
+        `when`(cursor.getString(0)).thenReturn("Animalia")
+        `when`(cursor.getString(1)).thenReturn("Ascidies")
+
+        // when getting a Taxonomy instance from Cursor
+        val taxonomy = fromCursor(cursor)
+
+        // then
+        assertNotNull(taxonomy)
+        assertEquals(Taxonomy("Animalia",
+                              "Ascidies"),
+                     taxonomy)
+    }
+
+    @Test
+    fun testParcelable() {
+        // given a Taxonomy instance
+        val taxonomy = Taxonomy("Animalia",
+                                "Ascidies")
+
+        // when we obtain a Parcel object to write the Taxonomy instance to it
+        val parcel = Parcel.obtain()
+        taxonomy.writeToParcel(parcel,
+                               0)
+
+        // reset the parcel for reading
+        parcel.setDataPosition(0)
+
+        // then
+        assertEquals(taxonomy,
+                     Taxonomy.CREATOR.createFromParcel(parcel))
+    }
+}
