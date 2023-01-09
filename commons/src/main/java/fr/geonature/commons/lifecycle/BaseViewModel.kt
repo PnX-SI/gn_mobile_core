a8add66cfdc7c7ff3495af917ea2eba2c11255dc
S. Grimault <sebastien.grimault@gmail.com>
2023-01-09 21:16:23 +0100

    feat: base view model with error handling

--- commons/src/main/java/fr/geonature/commons/lifecycle/BaseViewModel.kt
+++ commons/src/main/java/fr/geonature/commons/lifecycle/BaseViewModel.kt
@@ -16,7 +16,14 @@ abstract class BaseViewModel : ViewModel() {
     private val _failure: MutableLiveData<Failure> = MutableLiveData()
     val failure: LiveData<Failure> = _failure
 
+    private val _error: MutableLiveData<Throwable> = MutableLiveData()
+    val error: LiveData<Throwable> = _error
+
     protected fun handleFailure(failure: Failure) {
         _failure.value = failure
+    }
+
+    protected fun handleError(error: Throwable) {
+        _error.value = error
     }
 }
