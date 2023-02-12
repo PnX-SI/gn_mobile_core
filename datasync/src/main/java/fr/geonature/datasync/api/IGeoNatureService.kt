12146006fa327a5e6be61b74fd0a77a4d9b78682
S. Grimault <sebastien.grimault@gmail.com>
2023-02-12 15:26:48 +0100

    feat(https://github.com/PnX-SI/gn_mobile_occtax/issues/84): synchronize media files

--- datasync/src/main/java/fr/geonature/datasync/api/IGeoNatureService.kt
+++ datasync/src/main/java/fr/geonature/datasync/api/IGeoNatureService.kt
@@ -2,22 +2,28 @@ package fr.geonature.datasync.api
 
 import fr.geonature.datasync.api.model.AuthCredentials
 import fr.geonature.datasync.api.model.AuthLogin
+import fr.geonature.datasync.api.model.Media
 import fr.geonature.datasync.api.model.NomenclatureType
 import fr.geonature.datasync.api.model.TaxrefArea
 import fr.geonature.datasync.api.model.User
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
 import retrofit2.http.Path
 import retrofit2.http.Query
 import retrofit2.http.Streaming
 import retrofit2.http.Url
 
 /**
- * GeoNature API interface definition.
+ * _GeoNature_ API interface definition.
  *
  * @author S. Grimault
  */
@@ -31,6 +37,25 @@ interface IGeoNatureService {
     fun authLogin(
         @Body authCredentials: AuthCredentials
     ): Call<AuthLogin>
+
+    @Headers(
+        "Accept: application/json, text/plain, */*"
+    )
+    @POST("api/gn_commons/media")
+    @Multipart
+    fun sendMediaFile(
+        @Part("id_nomenclature_media_type") mediaType: Long,
+        @Part("id_table_location") tableLocation: Long,
+        @Part("author") author: RequestBody,
+        @Part("title_en") titleEn: RequestBody?,
+        @Part("title_fr") titleFr: RequestBody?,
+        @Part("description_en") descriptionEn: RequestBody?,
+        @Part("description_fr") descriptionFr: RequestBody?,
+        @Part mediaFile: MultipartBody.Part
+    ): Call<Media>
+
+    @DELETE("api/gn_commons/media/{id}")
+    fun deleteMediaFile(@Path("id") mediaId: Int): Call<ResponseBody>
 
     @Headers("Accept: application/json")
     @GET("api/meta/datasets?fields=modules")
@@ -63,6 +88,10 @@ interface IGeoNatureService {
     @Headers("Accept: application/json")
     @GET("api/gn_commons/t_mobile_apps")
     fun getApplications(): Call<ResponseBody>
+
+    @Headers("Accept: application/json")
+    @GET("api/gn_commons/get_id_table_location/pr_occtax.cor_counting_occtax")
+    fun getIdTableLocation(): Call<Long>
 
     @Streaming
     @GET
