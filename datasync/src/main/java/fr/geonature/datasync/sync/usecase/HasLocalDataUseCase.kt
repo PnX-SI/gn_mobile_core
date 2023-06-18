da630bd2db46148377fadfbe69e8e6c6ff5b38e4
S. Grimault <sebastien.grimault@gmail.com>
2023-06-18 10:25:48 +0200

    fix: use Dispatchers.IO when invoking database operations…

--- datasync/src/main/java/fr/geonature/datasync/sync/usecase/HasLocalDataUseCase.kt
+++ datasync/src/main/java/fr/geonature/datasync/sync/usecase/HasLocalDataUseCase.kt
@@ -2,6 +2,8 @@ package fr.geonature.datasync.sync.usecase
 
 import fr.geonature.commons.data.LocalDatabase
 import fr.geonature.commons.interactor.BaseResultUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 /**
@@ -12,19 +14,20 @@ import javax.inject.Inject
 class HasLocalDataUseCase @Inject constructor(private val database: LocalDatabase) :
     BaseResultUseCase<Boolean, BaseResultUseCase.None>() {
 
-    override suspend fun run(params: None): Result<Boolean> {
-        return runCatching {
-            !database
-                .inputObserverDao()
-                .isEmpty() && !database
-                .taxonomyDao()
-                .isEmpty() && !database
-                .datasetDao()
-                .isEmpty() && !database
-                .nomenclatureTypeDao()
-                .isEmpty() && !database
-                .taxonDao()
-                .isEmpty()
+    override suspend fun run(params: None): Result<Boolean> =
+        withContext(Dispatchers.IO) {
+            runCatching {
+                !database
+                    .inputObserverDao()
+                    .isEmpty() && !database
+                    .taxonomyDao()
+                    .isEmpty() && !database
+                    .datasetDao()
+                    .isEmpty() && !database
+                    .nomenclatureTypeDao()
+                    .isEmpty() && !database
+                    .taxonDao()
+                    .isEmpty()
+            }
         }
-    }
 }
